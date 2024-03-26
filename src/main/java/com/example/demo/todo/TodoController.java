f3d6989161bc935c3c33bf278909f0f890fec1ef
Aryan Kumar Singh <aryankumarsingh1503@gmail.com>
2024-03-27 01:18:03 +0530

    validations

--- src/main/java/com/example/demo/todo/TodoController.java
+++ src/main/java/com/example/demo/todo/TodoController.java
@@ -50,4 +50,11 @@ public class TodoController {
                 LocalDate.now().plusYears(1), false);
         return "redirect:list-todos";
     }
+
+    @RequestMapping("delete-todo")
+    public String deleteTodo(@RequestParam int id) {
+        todoService.deleteById(id);
+        return "redirect:list-todos";
+
+    }
 }
--- src/main/java/com/example/demo/todo/TodoService.java
+++ src/main/java/com/example/demo/todo/TodoService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+
 @Service
 public class TodoService {
     private static int todosCount = 0;
@@ -23,5 +25,9 @@ public class TodoService {
 
         Todo todo = new Todo(++todosCount,username,description,targetDate,done);
         todos.add(todo);
+    }
+    public void deleteById(int id) {
+        Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        todos.removeIf(predicate);
     }
 }
